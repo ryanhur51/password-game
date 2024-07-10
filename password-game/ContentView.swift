1ee51daef6731f8c07f1ec5dd8cdcce2faa46a6a
ryanhur51 <ryanhur51@gmail.com>
2024-07-10 17:04:16 -0400

    test commit

--- password-game/ContentView.swift
+++ password-game/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var password = "Enter your password here"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack{
+                Text("Password Game")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    
+                TextEditor(text: $password)
+                    .opacity(0.5)
+                    .frame(width: 350, height: 100, alignment: .top)
+                    .background(Color.red)
+                    .cornerRadius(10)
+                    .position(x: 200, y: 75)
+            }
         }
-        .padding()
     }
 }
 
