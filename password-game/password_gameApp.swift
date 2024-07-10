7aa9ee01d53579e2e156fed229a323b1e8f0f9d3
ryanhur51 <ryanhur51@gmail.com>
2024-07-10 16:19:45 -0400

    Initial Commit

---
+++ password-game/password_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  password_gameApp.swift
+//  password-game
+//
+//  Created by Ryan Hur on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct password_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
