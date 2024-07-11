34c838b6e1aad26f17021cd813007612862432ed
ryanhur51 <ryanhur51@gmail.com>
2024-07-11 15:43:47 -0400

    added dome UI and game logic

--- password-game/ContentView.swift
+++ password-game/ContentView.swift
@@ -1,27 +1,68 @@
-//
-//  ContentView.swift
-//  password-game
-//
-//  Created by Ryan Hur on 7/10/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    @State private var password = "Enter your password here"
+    @State var password = ""
+    
+    @State var r1 = "Rule 1: Password must be at least 5 characters."
+    @State var r1Color = Color.primary
+    @State var r2 = "Rule 2: Password must have a number."
+    @State var r2Color = Color.primary
+    @State var flag = false
+    
     var body: some View {
         NavigationView {
-            VStack{
+            VStack {
                 Text("Password Game")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .font(.title)
                     .padding()
-                    
+                Text("Please choose a password")
+                    .multilineTextAlignment(.leading)
+                    .padding(.bottom)
                 TextEditor(text: $password)
-                    .opacity(0.5)
-                    .frame(width: 350, height: 100, alignment: .top)
-                    .background(Color.red)
-                    .cornerRadius(10)
-                    .position(x: 200, y: 75)
+                    .frame(width: 350, height: 50, alignment: .center)
+                    .background(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.black, lineWidth: 10)
+                    )
+                    .onChange(of: password) {
+                        if rule1() == true{
+                            flag = true
+                        }
+                    }
+                
+                Text(r1)
+                    .foregroundColor(r1Color)
+                    .padding()
+                
+                if flag == true {
+                    Text(r2)
+                        .foregroundColor(r2Color)
+                }
+                
+
+                
+                Spacer()
+            }
+        }
+    }
+    
+    func rule1() -> Bool {
+        if password.count < 5 {
+            r1Color = Color.red
+            return false
+        } else {
+            r1Color = Color.green
+            rule2()
+            return true
+        }
+    }
+    
+    func rule2(){
+        for character in password {
+            if character.isNumber{
+                r2Color = Color.green
+            } else {
+                r2Color = Color.red
             }
         }
     }
